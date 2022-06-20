22a5ccea742a1a6fd698a30ee042a62bad6f5d8e
amc131 <76466256+amc131@users.noreply.github.com>
2022-06-19 17:49:50 -0700

    Updated for 1.6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import { useState } from 'react'
 
+const Display = props => <div>{props.value}</div>
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+   {props.text}
+  </button>
+)
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const setToValue = (rating, other) => {
+      other(rating + 1)
+  } 
+  
+
   return (
     <div>
+
       <h1>give feedback</h1>
+      <Button handleClick={() => setToValue(good, setGood)} text="good" />
+      <Button handleClick ={() => setToValue(neutral, setNeutral)} text="neutral" />
+      <Button handleClick ={() => setToValue(bad, setBad)} text="bad" />
+      <h1>statistics</h1>
+      <div>good {good}</div> 
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+
       </div>
   )
 }
